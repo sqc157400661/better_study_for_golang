22344cbc2c7c5e6bde694edd4eb93af0eee7869b
SQC <sqc157400661@163.com>
2020-05-10 21:29:40 +0800

    性能分析和优化

---
+++ 10/3.go
@@ -0,0 +1,60 @@
+/**
+Go性能调优 - Pprof
+runtime/pprof：采集工具型应用运行数据进行分析
+net/http/pprof：采集服务型应用运行时数据进行分析
+*/
+// runtime_pprof/main.go
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"runtime/pprof"
+	"time"
+)
+
+// 一段有问题的代码
+func logicCode() {
+	var c chan int
+	for {
+		select {
+		case v := <-c:
+			fmt.Printf("recv from chan, value:%v\n", v)
+		default:
+
+		}
+	}
+}
+
+func main() {
+	var isCPUPprof bool
+	var isMemPprof bool
+
+	flag.BoolVar(&isCPUPprof, "cpu", false, "turn cpu pprof on")
+	flag.BoolVar(&isMemPprof, "mem", false, "turn mem pprof on")
+	flag.Parse()
+
+	if isCPUPprof {
+		file, err := os.Create("./cpu.pprof")
+		if err != nil {
+			fmt.Printf("create cpu pprof failed, err:%v\n", err)
+			return
+		}
+		pprof.StartCPUProfile(file)
+		defer pprof.StopCPUProfile()
+	}
+	for i := 0; i < 8; i++ {
+		go logicCode()
+	}
+	time.Sleep(20 * time.Second)
+	if isMemPprof {
+		file, err := os.Create("./mem.pprof")
+		if err != nil {
+			fmt.Printf("create mem pprof failed, err:%v\n", err)
+			return
+		}
+		pprof.WriteHeapProfile(file)
+		file.Close()
+	}
+}
