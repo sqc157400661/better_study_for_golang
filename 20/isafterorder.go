0f2ee27bfd309874beca1cbd8be74b0baf75361d
sqc157400661@163.com <sqc157400661@163.com>
2020-07-17 17:18:02 +0800

    如何判断一个数组是否是二元查找树后序遍历的序列

--- 20/isafterorder.go
+++ 20/isafterorder.go
@@ -1,5 +1,5 @@
 /*
-	如何把二叉树转换为双向链表
+	如何判断一个数组是否是二元查找树后序遍历的序列
  */
 package main
 
@@ -12,62 +12,50 @@ type BNode struct{
 	RightChild *BNode
 }
 
-var pHead *BNode
-var pEnd *BNode
 
 func main(){
-	data :=[]int{1,2,3,4,5,6,7,8,9,10}
+	data :=[]int{1,3,2,5,7,6,4}
 	fmt.Println("数组:",data)
-	root := arrayToTree(data,0,len(data)-1)
-	InOrderBSTree(root)
-	fmt.Print("转换后双向链表正向遍历：")
-	for cur:=pHead;cur !=nil;cur=cur.RightChild{
-		fmt.Print(cur.Data," ")
+	result := IsAfterOrder(data,0,len(data)-1)
+	if result {
+		fmt.Println("是二元查找树的后续遍历序列")
 	}
-	fmt.Println()
-	fmt.Print("转换后双向链表逆向遍历：")
-	for cur:=pEnd;cur !=nil;cur=cur.LeftChild{
-		fmt.Print(cur.Data," ")
-	}
-}
-// 有序数组转成树结构
-func arrayToTree(arr []int,start int,end int) *BNode{
-	var root *BNode
-	if end >= start{
-		root = &BNode{}
-		mid := (start + end +1)/2
-		// 树的根节点为数组的中间元素
-		root.Data = arr[mid]
-		// 递归的用左半部分数据构造root的左子树
-		root.LeftChild = arrayToTree(arr,start,mid-1)
-		// 递归的用右半部分数据构造root的右子树
-		root.RightChild = arrayToTree(arr,mid+1,end)
-	}
-	return root
 }
 
+
 /*
-	方法功能：把二叉树转换成双向链表
-	输入参数：root：二叉树跟节点
+	方法功能：判断一个数组是否是二元查找树的后续遍历序列
+	输入参数：arr：数组
  */
-func InOrderBSTree(root *BNode){
-	if root == nil {
-		return
+func IsAfterOrder(arr []int,start int,end int) bool{
+	if arr == nil {
+		return false
 	}
-	// 转换root的左子树
-	InOrderBSTree(root.LeftChild)
-	// 使用当前节点的左孩子指向双向链表中农的最后一个结点
-	root.LeftChild = pEnd
-	// 双向列表为空，当前遍历的结点为双向链表的头结点
-	if pEnd == nil {
-		pHead = root
-	}else{
-		// 使双向链表中最后一个结点的右孩子指向当前结点
-		pEnd.RightChild = root
+	// 数组的最后一个结点必定是跟节点
+	root := arr[end]
+	var i,j int
+	// 找到一个大于root的值，那么前面所有的结点都位于root的左子树上
+	for i = start;i<end;i++{
+		if arr[i] > root {
+			break
+		}
 	}
-	// 将当前结点设为双向链表中最后一个结点
-	pEnd = root
-	// 转换root的右字数
-	InOrderBSTree(root.RightChild)
+	// 如果序列是后续遍历的序列，那么i开始的所有值都应该大于根结点root的值
+	for j=i;j<end;j++ {
+		if arr[j] < root {
+			return false
+		}
+	}
+	leftIsAfterOrder := true
+	rightIsAfterOrder := true
+	// 判断小于root值序列是否是某一二元查找树的后续遍历
+	if i>start{
+		leftIsAfterOrder = IsAfterOrder(arr,start,i-1)
+	}
+	// 判断大于root值的序列是否是某二元查找树的后续遍历
+	if j<end{
+		rightIsAfterOrder = IsAfterOrder(arr,i,end)
+	}
+	return leftIsAfterOrder && rightIsAfterOrder
 }
 
