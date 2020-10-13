be985a767d05525d17c6e81499426bb932b78e8d
sqc <sqc157400661@163.com>
2020-10-13 09:05:36 +0800

    临时提交

--- 每日一题/202009/11.go
+++ 每日一题/202009/11.go
@@ -1,1 +1,79 @@
 package main
+
+import "fmt"
+
+//链表定义
+type ListNode struct {
+	Val  int       // 数据域
+	Next *ListNode //  对下一个节点的作用
+}
+
+//创建测试链表
+func CreateTestNode(l1 *ListNode, l2 *ListNode) {
+	l1.Next = &ListNode{}
+	l1.Next.Val = 1
+	l1.Next.Next = &ListNode{}
+	l1.Next.Next.Val = 2
+	l1.Next.Next.Next = &ListNode{}
+	l1.Next.Next.Next.Val = 3
+	l1.Next.Next.Next.Next = &ListNode{}
+	l1.Next.Next.Next.Next.Val = 3
+	l1.Next.Next.Next.Next.Next = &ListNode{}
+	l1.Next.Next.Next.Next.Next.Val = 4
+	l1.Next.Next.Next.Next.Next.Next = &ListNode{}
+	l1.Next.Next.Next.Next.Next.Next.Val = 4
+	l1.Next.Next.Next.Next.Next.Next.Next = &ListNode{}
+	l1.Next.Next.Next.Next.Next.Next.Next.Val = 5
+
+	l2.Next = &ListNode{}
+	l2.Next.Val = 1
+	l2.Next.Next = &ListNode{}
+	l2.Next.Next.Val = 1
+	l2.Next.Next.Next = &ListNode{}
+	l2.Next.Next.Next.Val = 1
+	l2.Next.Next.Next.Next = &ListNode{}
+	l2.Next.Next.Next.Next.Val = 2
+	l2.Next.Next.Next.Next.Next = &ListNode{}
+	l2.Next.Next.Next.Next.Next.Val = 3
+}
+func main() {
+	var l1 = &ListNode{}
+	var l2 = &ListNode{}
+	CreateTestNode(l1, l2)
+	PrintNode("l1: ", l1)
+	PrintNode("l2: ", l2)
+	PrintNode("去除重复l1: ", deleteDuplicates(l1))
+	PrintNode("去除重复l2: ",  deleteDuplicates(l2))
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	node := head
+	for node != nil{
+		node2 := node
+		for node2.Next != nil{
+			if(node.Val == node2.Next.Val){
+				node2.Next = node2.Next.Next
+			}else{
+				node2 = node2.Next
+			}
+
+		}
+		node = node.Next
+	}
+	return head
+}
+//打印链表的方法
+func PrintNode(info string, node *ListNode) {
+	fmt.Print(info)
+	for cur := node.Next; cur != nil; cur = cur.Next {
+		fmt.Print(cur.Val, " ")
+	}
+	fmt.Println()
+}
