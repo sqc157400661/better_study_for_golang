2e8aea8156f0e53b36cb1ee6a8148d898456d9a8
sqc <sqc157400661@163.com>
2020-10-27 17:53:31 +0800

    1

--- 每日一题/202009/11.go
+++ 每日一题/202009/11.go
@@ -43,7 +43,7 @@ func main() {
 	PrintNode("l1: ", l1)
 	PrintNode("l2: ", l2)
 	PrintNode("去除重复l1: ", deleteDuplicates(l1))
-	PrintNode("去除重复l2: ",  deleteDuplicates(l2))
+	//PrintNode("去除重复l2: ",  deleteDuplicates(l2))
 }
 
 /**
@@ -54,20 +54,23 @@ func main() {
  * }
  */
 func deleteDuplicates(head *ListNode) *ListNode {
-	node := head
-	for node != nil{
-		node2 := node
+	node := &ListNode{Next:head}
+	node1 := node
+	var eq int
+	for node.Next != nil{
+		node2 := node.Next
 		for node2.Next != nil{
-			if(node.Val == node2.Next.Val){
-				node2.Next = node2.Next.Next
+			if(node.Next.Val == node2.Next.Val || eq ==node2.Next.Val){
+				fmt.Println(node.Next.Val)
+				eq = node2.Next.Val
+				node2 = node2.Next.Next
 			}else{
 				node2 = node2.Next
 			}
-
 		}
 		node = node.Next
 	}
-	return head
+	return node1.Next
 }
 //打印链表的方法
 func PrintNode(info string, node *ListNode) {
--- 每日一题/202009/12.go
+++ 每日一题/202009/12.go
@@ -3,11 +3,38 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"runtime"
+	"time"
 )
 
 func main() {
-	a :=""
-	re := strings.Split(a,"_")
-	fmt.Println(re[0])
+	//defer func() {
+	//	if e:=recover();e !=nil{
+	//		fmt.Println(e)
+	//	}
+	//}()
+	go get()
+	go put()
+	time.Sleep(time.Second * 10)
+	fmt.Println(111111)
+}
+
+func get(){
+	defer func() {
+		if e:=recover();e !=nil{
+			//fmt.Println(e)
+			var buf [4096]byte
+			n := runtime.Stack(buf[:],false)
+			fmt.Println(e,string(buf[:n]))
+			//const size = 64 << 10
+			//buf := make([]byte, size)
+			//buf = buf[:runtime.Stack(buf, false)]
+			//fmt.Println(string(buf))
+		}
+	}()
+	panic(111)
+}
+
+func put(){
+	fmt.Println(3333)
 }
