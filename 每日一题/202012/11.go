3ed74b47846dbf696e76343b2bc610bf55e60d2a
sqc <sqc157400661@163.com>
2020-12-23 15:45:32 +0800

    数组中出现次数超过一半的数字

---
+++ 每日一题/202012/11.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+/**
+ *
+ * @param numbers int整型一维数组
+ * @return int整型
+ */
+func MoreThanHalfNum_Solution( numbers []int ) int {
+	var x,votes int
+	for i:=0;i< len(numbers);i++{
+		if votes ==0 {
+			x = numbers[i]
+			votes = 1
+		}else{
+			if x == numbers[i]{
+				votes +=1
+			}else{
+				votes -=1
+			}
+		}
+	}
+	return x
+}
+
+func main(){
+	var arr = []int{1,2,3,2,2,2,5,4,2}
+	var arr1 = []int{3,3,4,8,8,8,8}
+	fmt.Println(MoreThanHalfNum_Solution(arr))
+	fmt.Println(MoreThanHalfNum_Solution(arr1))
+}
