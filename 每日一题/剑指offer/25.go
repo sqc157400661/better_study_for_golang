65054cdf7b1c97c3498be9a31eef26599db0d8e0
sqc <sqc157400661@163.com>
2021-01-07 11:00:07 +0800

    合并两个排序的链表

---
+++ 每日一题/剑指offer/25.go
@@ -0,0 +1,35 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	var dum = &ListNode{}
+	cur := dum
+	for l1 != nil && l2 !=nil {
+		if l1.Val <= l2.Val{
+			cur.Next = l1
+			l1 = l1.Next
+		}else{
+			cur.Next = l2
+			l2 = l2.Next
+		}
+		cur = cur.Next
+	}
+	if l1 == nil && l2 !=nil{
+		cur.Next = l2
+	}
+	if l2 == nil && l1 !=nil{
+		cur.Next = l1
+	}
+	return dum.Next
+}
