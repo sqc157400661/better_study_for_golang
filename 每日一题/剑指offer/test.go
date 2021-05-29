8e5637ad095badd534b6a09d211d6c9e15d2d45a
shiqc <157400661@qq.com>
2021-05-29 18:13:25 +0800

    4554

--- 每日一题/剑指offer/test.go
+++ 每日一题/剑指offer/test.go
@@ -1,19 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
 
+// Main Goroutine
 func main() {
-	t1 := []int{1,3}
-	testSlice(t1)
-
-	t2 := map[int]int{1:2}
-	testMap(t2)
-
-	fmt.Println(t1,t2)
-}
-func testSlice(s []int){
-	s = append(s,5,6)
-}
-func testMap(s map[int]int){
-	s[55] = 55
+	// 模拟单核 CPU
+	runtime.GOMAXPROCS(1)
+	go func() {
+		for true {
+			fmt.Println("11212")
+		}
+	}()
+	time.Sleep(time.Millisecond)
+	fmt.Println("shiqingchuang")
 }
